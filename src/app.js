96d1c8aa21a34780d9cd75b98ca46ec83cbb2459
Nathan Green <hello@nathgreen.co.uk>
2019-01-16 15:52:14 +0000

    add helmet and compression middlewares

--- src/app.js
+++ src/app.js
@@ -2,16 +2,20 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
+const compression = require('compression');
+const helmet = require('helmet');
 
 const routes = require('./routes');
 
 const app = express();
 
 app
-  .use(cors())
+  .use(helmet()) // security middlewares
+  .use(cors()) // public API, allow all origins
   .use(bodyParser.json())
   .use(bodyParser.urlencoded({ extended: true }))
   .use(cookieParser())
+  .use(compression()) // enable gzip compression
   .use('/api/v1', routes)
   .all('/*', (req, res) => res.status(200).send('Please read the API documentation'));
 
