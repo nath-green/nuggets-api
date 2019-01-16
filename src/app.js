b354d48e4b40d11e338995b12d9a9b14a6098391
Nathan Green <hello@nathgreen.co.uk>
2019-01-16 12:15:26 +0000

    added cors

--- src/app.js
+++ src/app.js
@@ -1,14 +1,19 @@
 const express = require('express');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 
+const config = require('../config');
 const routes = require('./routes');
 
 const app = express();
 
-app.use(cookieParser());
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+
+app.use(cookieParser());
+
 app.use('/api/v1', routes);
 
 app.all('/*', (req, res) => res.status(200).send('Please read the API documentation'));
