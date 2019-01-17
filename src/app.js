8f01f217f658ed09fd9706a8f24033e486b1a971
Nathan Green <hello@nathgreen.co.uk>
2019-01-17 08:33:41 +0000

    add rate limiter middleware

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const compression = require('compression');
 const helmet = require('helmet');
+const rateLimit = require('express-rate-limit');
 
 const routes = require('./routes');
 
@@ -12,11 +13,13 @@ const app = express();
 app
   .use(helmet()) // security middlewares
   .use(cors()) // public API, allow all origins
-  .use(bodyParser.json())
-  .use(bodyParser.urlencoded({ extended: true }))
-  .use(cookieParser())
+  .use(bodyParser.json()) // parse requests of content-type: application/json
+  .use(bodyParser.urlencoded({ extended: true })) // parse requests of content-type: application/x-www-form-urlencoded
+  .use(cookieParser()) // parse Cookie header and populate req.cookies
   .use(compression()) // enable gzip compression
-  .use('/api/v1', routes)
-  .all('/*', (req, res) => res.status(200).send('Please read the API documentation'));
+  .enable('trust proxy') // express-rate-limit, needed if behind a reverse proxy (Heroku, AWS ELB etc)
+  .use(rateLimit()) // enable default API limits (5 in 1 minute)
+  .use('/api/v1', routes) // define routes with versioned prefix
+  .all('/*', (req, res) => res.status(200).send('Please read the API documentation')); // catch all route
 
 module.exports = app;
