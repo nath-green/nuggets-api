1cf7e4baa28e5304084b6703cf08cf2afa74bd5d
Nathan Green <hello@nathgreen.co.uk>
2019-01-25 15:36:16 +0000

    added fields to model

--- src/models/roster.model.js
+++ src/models/roster.model.js
@@ -1,8 +1,16 @@
 const mongoose = require('mongoose');
 
 const RosterSchema = new mongoose.Schema({
-  name: String,
-  jersey: Number
+  name: { type: String, required: true },
+  jersey: { type: Number, required: true },
+  position: { type: String, required: true },
+  country: { type: String, required: true },
+  yearsPro: { type: Number, required: true },
+  stats: {
+    ppg: { type: Number, required: true },
+    rpg: { type: Number, required: true },
+    apg: { type: Number, required: true }
+  }
 });
 
 module.exports = mongoose.model('Roster', RosterSchema, 'roster');
