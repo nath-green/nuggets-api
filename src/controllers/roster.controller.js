fbcede385f777607b3d88823a552822f4c1416a9
Nathan Green <hello@nathgreen.co.uk>
2019-01-25 09:27:29 +0000

    added update by id controller and updates to delete controller

--- src/controllers/roster.controller.js
+++ src/controllers/roster.controller.js
@@ -30,20 +30,25 @@ exports.getById = (req, res, next) => {
   }).select('-__v');
 };
 
-// exports.updateById = (req, res, next) => {
-//   Roster.findById(req.params.id, (err, player) => {
-//     if (err) return res.status(400).send({ success: false, message: 'That ID does not exist.' });
-//     return res.status(200).send({ data: player });
-//   }).select('-__v');
-// };
+exports.updateById = (req, res, next) => {
+  Roster.findByIdAndUpdate(
+    req.params.id,
+    { $set: { name: req.body.name, jersey: req.body.jersey } },
+    { new: true },
+    (err, player) => {
+      if (err || player === null)
+        return res.status(400).send({ success: false, message: 'That ID does not exist.' });
+      return res.status(200).send({ sucess: true, message: 'Player updated', data: player });
+    }
+  );
+};
 
 exports.deleteById = (req, res, next) => {
-  Roster.findById(req.params.id, (err, player) => {
+  Roster.findByIdAndRemove(req.params.id, (err, player) => {
     if (err || player === null)
       return res.status(400).send({ success: false, message: 'That ID does not exist.' });
-    return Roster.findOneAndDelete(req.params.id, errFind => {
-      if (errFind) return res.status(400).send({ success: false });
-      return res.status(200).send({ sucess: true, message: 'Player deleted from roster' });
-    });
+    return res
+      .status(200)
+      .send({ sucess: true, message: 'Player deleted from roster', data: player });
   });
 };
--- src/routes/roster.routes.js
+++ src/routes/roster.routes.js
@@ -5,7 +5,7 @@ const Roster = require('../controllers/roster.controller');
 router.route('/').get(Roster.list);
 router.route('/:id').get(Roster.getById);
 router.route('/').post(auth.hasRole(2), Roster.add);
-// router.route('/:id').patch(auth.hasRole(2), Roster.updateById);
+router.route('/:id').patch(auth.hasRole(2), Roster.updateById);
 router.route('/:id').delete(auth.hasRole(2), Roster.deleteById);
 
 module.exports = router;
